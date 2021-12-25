2052ee1d5dcab2f3eb58fa3f839475f4539f9d24
Esneider Ospina <sneuder8@gmail.com>
2021-12-25 18:06:17 -0500

    Added js for change icon color. Icons added. New filed text style and transitions

---
+++ scss/functions_js/changeStyles.js
@@ -0,0 +1,15 @@
+function focus_iconColor1(){
+    document.getElementById("icon1").style.color = "#009a25";
+}
+
+function focusout_iconColor1(){
+    document.getElementById("icon1").style.color = "#d2d2d2";
+}
+
+function focus_iconColor2(){
+    document.getElementById("icon2").style.color = "#009a25";
+}
+
+function focusout_iconColor2(){
+    document.getElementById("icon2").style.color = "#d2d2d2";
+}
