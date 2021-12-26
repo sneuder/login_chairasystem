a103bd621ef3236cfeef6c8e511963c5af72571f
Esneider Ospina <sneuder8@gmail.com>
2021-12-25 21:46:26 -0500

    Animation added (bottom of type form), submit button, border of Dont close session

--- scss/functions_js/changeStyles.js
+++ scss/functions_js/changeStyles.js
@@ -1,15 +1,19 @@
 function focus_iconColor1(){
     document.getElementById("icon1").style.color = "#009a25";
+    document.getElementById("user").style.borderBottomColor = "#009a25";
 }
 
 function focusout_iconColor1(){
     document.getElementById("icon1").style.color = "#d2d2d2";
+    document.getElementById("user").style.borderBottomColor = "#d2d2d2";
 }
 
 function focus_iconColor2(){
     document.getElementById("icon2").style.color = "#009a25";
+    document.getElementById("password").style.borderBottomColor = "#009a25";
 }
 
 function focusout_iconColor2(){
     document.getElementById("icon2").style.color = "#d2d2d2";
+    document.getElementById("password").style.borderBottomColor = "#d2d2d2";
 }
