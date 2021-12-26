c3697118ff0084eefcff98276c683f8b0223d083
Esneider Ospina <sneuder8@gmail.com>
2021-12-26 10:49:39 -0500

    Comments on HTML and JS

--- scss/functions_js/changeStyles.js
+++ scss/functions_js/changeStyles.js
@@ -1,18 +1,22 @@
+//When the fiel text is focused
 function focus_iconColor1(){
     document.getElementById("icon1").style.color = "#009a25";
     document.getElementById("user").style.borderBottomColor = "#009a25";
 }
 
+//When the fiel text isnt focused
 function focusout_iconColor1(){
     document.getElementById("icon1").style.color = "#d2d2d2";
     document.getElementById("user").style.borderBottomColor = "#d2d2d2";
 }
 
+//When the fiel password isnt focused
 function focus_iconColor2(){
     document.getElementById("icon2").style.color = "#009a25";
     document.getElementById("password").style.borderBottomColor = "#009a25";
 }
 
+//When the filed password isnt focused
 function focusout_iconColor2(){
     document.getElementById("icon2").style.color = "#d2d2d2";
     document.getElementById("password").style.borderBottomColor = "#d2d2d2";
